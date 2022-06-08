ca48632f9f9d51f478c7e80d641569675e7d55dd
igorfltv <38211024+igorfltv@users.noreply.github.com>
2022-06-08 20:37:58 +0300

    tests
    tests addition

---
+++ functions.py
@@ -0,0 +1,13 @@
+class Functions:
+
+    def __init__(self):
+        pass
+
+    def plus(self, a, b):
+        return a+b
+
+    def minus(self, a, b):
+        return a-b
+
+    def div(self, a, b):
+        return a/b
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello jenkins")
+import unittest
+from functions import Functions
+
+class TestFunction(unittest.TestCase):
+    def setUp(self):
+        self.functions = Functions()
+    def test_add(self):
+        self.assertEqual(self.functions.plus(4,7), 11)
+    def test_minus(self):
+        self.assertEqual(self.functions.minus(10,2), 8)
+    def test_divide(self):
+        self.assertEqual(self.functions.div(20,4), 5)
+
+if __name__ == "__main__":
+  unittest.main()
